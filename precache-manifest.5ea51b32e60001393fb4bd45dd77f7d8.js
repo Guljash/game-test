3b5b5ee36b90ad9f808dccd8736f5a7354993dec
George Antonov <guljash.main@gmail.com>
2020-01-29 20:07:31 +0300

    Updates

---
+++ precache-manifest.5ea51b32e60001393fb4bd45dd77f7d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03a1a6287f0234ef2724d6be1be8935",
+    "url": "/game-test/index.html"
+  },
+  {
+    "revision": "678cf9481a25e7be5d41",
+    "url": "/game-test/static/css/main.0ade82df.chunk.css"
+  },
+  {
+    "revision": "b1e7bdb972a330591847",
+    "url": "/game-test/static/js/2.e2ba7987.chunk.js"
+  },
+  {
+    "revision": "3aed3ae0aff2303b02de41c27714496b",
+    "url": "/game-test/static/js/2.e2ba7987.chunk.js.LICENSE"
+  },
+  {
+    "revision": "678cf9481a25e7be5d41",
+    "url": "/game-test/static/js/main.ac4ccaf6.chunk.js"
+  },
+  {
+    "revision": "f523badf66b638e39ed0",
+    "url": "/game-test/static/js/runtime-main.298a323b.js"
+  }
+]);
